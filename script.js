434c83b1983e0eeb180ba999f3f19ccc9399288d
Yousef <yousefomar6464@gmail.com>
2023-12-08 16:19:22 +0200

    new2 edit

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 
 const menu = document.getElementById("menu");
 const action = document.getElementById("action");
+const ho = document.getElementById("ho");
+const ab = document.getElementById("ab");
+const se = document.getElementById("se");
+const sk = document.getElementById("sk");
+const co = document.getElementById("co");
 
 menu.addEventListener("click", ()=>{
     handleMenu();
 })
+
+ho.addEventListener("click", ()=>{
+    handleMenu();
+})
+ab.addEventListener("click", ()=>{
+    handleMenu();
+})
+se.addEventListener("click", ()=>{
+    handleMenu();
+})
+sk.addEventListener("click", ()=>{
+    handleMenu();
+})
+co.addEventListener("click", ()=>{
+    handleMenu();
+})
+
+
 function handleMenu(){
     menu.classList.toggle("is-active");
     action.classList.toggle("is-active");
+    ho.classList.toggle("is-active");
 }
 
 
